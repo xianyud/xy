532c5f85c145ea17b591d93ea4299fba89775330
xianyud <53156301+xianyud@users.noreply.github.com>
2019-07-29 09:48:38 +0800

    Add files via upload

---
+++ js/smile.js
@@ -0,0 +1,22 @@
+function smile() {
+    alert(':) 开心每一天')
+}
+function zhexue() {
+    window.open('https://www.bilibili.com/video/av10275558')
+}
+function sx() {
+    alert(':D 要开心吖')
+}
+function zan() {
+    alert('谢谢支持！')
+}
+function cai() {
+    alert('我会做得更好:)')
+}
+// 常用网站
+function tp() {
+    window.open('https://www.taobao.com/')
+}
+function youtuber() {
+    window.open('https://www.youtube.com/')
+}
