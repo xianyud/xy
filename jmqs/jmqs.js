30d8b9d34eeb85f1548f1dd5c2e35d8abaefb542
xianyud <53156301+xianyud@users.noreply.github.com>
2019-08-31 13:22:31 +0800

    Add files via upload

---
+++ jmqs/jmqs.js
@@ -0,0 +1,9 @@
+function bzhan() {
+    window.open('https://www.bilibili.com/video/av66063348?zw')
+}
+function zzheshouye() {
+    window.open('https://space.bilibili.com/173972934')
+}
+function huidao() {
+    window.open('../../index.html')
+}
